0c9b4070d431b68c4e21402e65d409caae1e439d
Afrah Aden <afrahaden05@gmail.com>
2023-07-25 15:46:10 -0700

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""
-place holder
-"""
-
+"""Exports to-do list info of all employess to JSON format."""
+import json
+import requests
 
 if __name__ == "__main__":
-        import requests
-            import json
-                users = requests.get(
-                                "https://jsonplaceholder.typicode.com/users")
-                    users = users.json()
-                        result = {}
-                            for user in users:
-                                        todos = requests.get(
-                                                            "https://jsonplaceholder.typicode.com/todos?userId={}"
-                                                                        .format(user["id"]))
-                                                todos = todos.json()
-                                                        result[user["id"]] = []
-                                                                for todo in todos:
-                                                                                result[user["id"]].append(
-                                                                                                        {"username": user["username"],
-                                                                                                                                "task": todo["title"], "completed": todo["completed"]})
-                                                                                                            with open("todo_all_employees.json", 'w') as result_file:
-                                                                                                                        json.dump(result, result_file)
+        url = "https://jsonplaceholder.typicode.com/"
+            users = requests.get(url + "users").json()
+
+                with open("todo_all_employees.json", "w", encoding="utf-8") as jsonfile:
+                            json.dump({
+                                            u.get("id"): [{
+                                                                "username": u.get("username"),
+                                                                                "task": t.get("title"),
+                                                                                                "completed": t.get("completed")
+                                                                                                                } for t in requests.get(url + "todos",
+                                                                                                                                                            params={"userId": u.get("id")}).json()]
+                                                                                                                            for u in users}, jsonfile)
